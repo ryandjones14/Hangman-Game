c0116cec92e9f0302b14ca236d855b845263454f
unknown <masonjterry@gmail.com>
2017-11-10 10:04:20 -0600

    semi final project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,67 @@
-// Create variables and arrays
-
-var wordBank = ["football", "baseball", "basketball", "soccer"];
-
+// word bank
+var wordBank = ["nintendo", "atari", "sega", "pacman"];
+// scoreboard vars
 var wins = 0;
-
 var losses = 0;
-
-var guessesLeft = 10;
-
+// guesses vars
 var guess;
-
 var guesses = [];
+var guessesLeft = 12;
+// random word vars
+var randomWord;
+var randomWordArray = [];
+var blanks = [];
 
-var startKey;
+randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+console.log(randomWord);
+randomWordArray = randomWord.split("");
+console.log(randomWordArray);
+index = randomWordArray.length;
+console.log(index);
 
-var resetKey;
-
-var wordChoice;
-
-var num;
-
-var letters;
-
-var output = [];
-
-function gameStats() {
-	document.getElementById("wins").textContent = "Wins: " + wins;
-	document.getElementById("losses").textContent = "Losses: " + losses;
-	document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
-	document.getElementById("start").textContent = "Start guessing!";
-	document.getElementById("guesses").textContent = "Guesses: " + guesses;
+for (i = 0; i < index; i++) {
+	blanks.push(" _ ");
 }
 
-// Create a way for the user to interact
+console.log("blanks: " + blanks);
+
+function updateStats() {
+
+document.getElementById("wordBlanks").textContent = blanks;
+document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
+document.getElementById("guesses").textContent = "Guesses: " + guesses;
+}
+
+function checkGuess(input) {
+	for (i = 0; i < index; i++) {
+		if (guess.includes(randomWordArray[i])) {
+			blanks[i] = guess;
+			document.getElementById("wordBlanks").textContent = blanks;
+		}
+
+	} 
+
+	if (!guess.includes(randomWordArray[i])) {
+			guesses.push(guess);
+			guessesLeft--;
+			console.log("guesses left: " + guessesLeft);
+			console.log("Guesses: " + guesses);
+		}
+
+	if (blanks.toString() === randomWordArray.toString()) {
+		alert("You won!");
+		location.reload();
+	}
+
+	if (guessesLeft === 0) {
+		alert("Try again");
+		location.reload();
+	}
+}
 
 document.onkeyup = function(event) {
-	startKey = event.key;
-	if (startKey === "Enter") {
-		gameStats();
-
-// Random word picker
-
-		wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
-		letters = wordChoice.split("");
-		num = letters.length;
-		console.log(letters);
-		console.log("num: " + num);
-		console.log("Word choice: " + wordChoice);
-		document.getElementById("wordChoice").textContent = "Current Word: " + wordChoice;
-
-
-	// // for loop to a a blank space for each letter in the word
- //    for(var i = 0; i < num; i++) {
- //      output.push("_")
- //      console.log(output)
- //    }
-     
- //    // HTML       
- //    document.getElementById("wordBlankd").innerHTML = output.join(" ");
-
-
-
-
-// user starts guessing
-		document.onkeyup = function(event) {
-			guess = event.key;
-			guesses.push(guess);
-			console.log("User guess: " + guess);
-			guessesLeft--;
-			gameStats();
-			//document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
-			//document.getElementById("guesses").textContent = "Guesses picked: " + guesses;
-
-// if guesses left = 0, add 1 to loss
-			if (guessesLeft === 0) {
-				losses++
-				guessesLeft = 10;
-				gameStats();
-				//document.getElementById("losses").textContent = "Losses: " + losses;
-				//document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
-			}
-
-
-				if (losses === 1 || wins === 1) {
-					document.getElementById("start").textContent = "Would you like to play again? Press 'space'!";
-					document.onkeyup = function(event) {
-						resetKey = event.key;
-						if (resetKey === " ") {
-							location.reload();
-						}
-					}
-				}
-		}
-	}
+	guess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("Guess: " + guess);
+	checkGuess(guess);
+	updateStats();
 }
